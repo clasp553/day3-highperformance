25a6474ba817dde230c0ed497e254ab84bc87722
Claudia Spallacci <claudia.spallacci@kemi.uu.se>
2022-02-28 10:43:06 +0100

    Added exercise 2

--- matmult_numpy.py
+++ matmult_numpy.py
@@ -4,19 +4,17 @@ import numpy as np
 N = 250
 
 # NxN matrix
-X = np.random.rand(N,N)*100 #Matrix contains floats
+X = np.random.rand(N,N)*100
 X = X.astype(int)
-#print(X)
 
 # Nx(N+1) matrix
-Y = np.random.rand(N,N+1)*100
+Y = np.random.rand(N,N)*100
 Y = Y.astype(int)
-#print(Y)
 
 # result is Nx(N+1)
 @profile
-def matmul(x,y):
-    result = np.matmul(x,y)
+def matmul(mat1,mat2):
+    result = np.matmul(mat1,mat2)
     return result
 
 print(matmul(X,Y))
